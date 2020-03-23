3a3105eeeb267a749a74fe02a08e544f4d7be904
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-23 15:25:33 -0400

    added color for success & error message

--- runner.js
+++ runner.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 class Runner {
   constructor() {
@@ -8,6 +9,7 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
+      console.log(chalk.gray(`-----${file.shortName}`));
       const beforeEaches = [];
       global.beforeEach = fn => {
         beforeEaches.push(fn);
@@ -16,16 +18,16 @@ class Runner {
         beforeEaches.forEach(func => func());
         try {
           fn();
-          console.log(`OK- ${desc}`);
+          console.log(chalk.green(`OK- ${desc}`));
         } catch (err) {
-          console.log(`X- ${desc}`);
-          console.log('\t', err.message);
+          console.log(chalk.red(`X- ${desc}`));
+          console.log(chalk.red('\t', err.message));
         }
       };
       try {
         require(file.name);
       } catch (err) {
-        console.log(err);
+        console.log(chalk.red(err));
       }
     }
   }
@@ -37,7 +39,7 @@ class Runner {
       const stats = await fs.promises.lstat(filePath);
 
       if (stats.isFile() && file.includes('.test.js')) {
-        this.testFiles.push({ name: filePath });
+        this.testFiles.push({ name: filePath, shortName: file });
       } else if (stats.isDirectory()) {
         const childFiles = await fs.promises.readdir(filePath);
         files.push(...childFiles.map(f => path.join(file, f)));
