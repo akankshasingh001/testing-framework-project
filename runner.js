fecef63067e999fed6393da8eed14a4094674989
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-23 14:22:58 -0400

    Implemented Breadth first search

---
+++ runner.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+class Runner {
+  constructor() {
+    this.files = [];
+  }
+  async collectFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+    return files;
+  }
+}
+module.exports = Runner;
